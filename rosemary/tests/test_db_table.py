91461f5afab876debddb472186c31c15791b6b23
Miguel Galán Lerate <miggaller@alum.us.es>
2025-04-22 12:44:00 +0200

    fix: fix rosemary tests

--- rosemary/tests/test_db_table.py
+++ rosemary/tests/test_db_table.py
@@ -28,7 +28,7 @@ def test_db_table_valid_table(mock_run, mock_getenv, mock_env_vars):
 
     assert result.exit_code == 0
     mock_run.assert_called_once_with(
-        'mysql -hlocalhost -uuvlhubdb_user -puvlhubdb_password uvlhubdb_test -e "SELECT * FROM user LIMIT 5;"',
+        ['mysql', '-hlocalhost', '-uuvlhubdb_user', '-puvlhubdb_password', 'uvlhubdb_test', '-e', 'SELECT * FROM user LIMIT 5;'],
         check=True,
     )
 
